0532274f88cd365d2b9c76daee3d270fce37e8ab
George Song <george@monozuku.com>
2018-03-08 06:26:44 -0800

    Basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React, { Component } from "react";
-import { Jumbotron, Container } from "reactstrap";
+import React from "react";
+
+import Marquee from "./components/Marquee";
+import Venue from './components/Venue';
 
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Jumbotron fluid>
-          <Container fluid>
-            <h1 className="display-3">Fluid jumbotron</h1>
-            <p className="lead">
-              This is a modified jumbotron that occupies the entire horizontal
-              space of its parent.
-            </p>
-          </Container>
-        </Jumbotron>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <div>
+    <Marquee />
+    <Venue />
+  </div>
+);
---
+++ src/components/Marquee.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Jumbotron, Container } from "reactstrap";
+
+export default () => (
+  <Jumbotron fluid>
+    <Container fluid>
+      <h1 className="display-3">Sam & Sue’s 50th Anniversary</h1>
+      <p className="lead">
+        Saturday, June 23, 2018
+      </p>
+      <p className="lead">
+        Kalispell, Montana
+      </p>
+    </Container>
+  </Jumbotron>
+);
