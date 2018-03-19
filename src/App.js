cc6f7dfeb730768ce59f56c33986ea4a0b400605
George Song <george@monozuku.com>
2018-03-18 17:36:03 -0700

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container } from "reactstrap";
+import { Container, Nav, NavItem, NavLink } from "reactstrap";
 
 import ActivityList from "./components/ActivityList";
 import Airport from "./components/Airport";
@@ -13,31 +13,57 @@ import WindowSizeProvider from "./providers/WindowSizeProvider";
 
 import "./App.css";
 
-export default () => {
-  const backgroundColor1 = "rgba(64, 60, 127, 0.2)";
-  const backgroundColor2 = "rgba(182, 174, 71, 0.2)";
-  const sectionComponents = [
-    <Event />,
-    <Airport />,
-    <LodgingList />,
-    <FoodList />,
-    <ActivityList />,
-    <ContactUs />
-  ];
-  const sections = sectionComponents.map((item, i) => {
-    const bgColor = i % 2 === 0 ? backgroundColor1 : backgroundColor2;
-    const style = { backgroundColor: bgColor };
-    return (
-      <Container fluid className="p-md-4 py-4" style={style} key={i}>
-        {item}
-      </Container>
-    );
-  });
-
-  return (
+export default class App extends React.PureComponent {
+  render = () => (
     <WindowSizeProvider>
       <Header />
-      {sections}
+      <NavMenu sections={sections} onClick={this.scrollTo} />
+      {this.renderSections()}
     </WindowSizeProvider>
   );
+
+  renderSections = () => {
+    const renderedSections = Object.entries(sections).map(this.renderSection);
+    return renderedSections;
+  };
+
+  renderSection = ([ref, { component }], i) => {
+    const backgroundColor1 = "rgba(64, 60, 127, 0.2)";
+    const backgroundColor2 = "rgba(182, 174, 71, 0.2)";
+    const bgColor = i % 2 === 0 ? backgroundColor1 : backgroundColor2;
+    const style = { backgroundColor: bgColor };
+
+    return (
+      <div ref={el => (this[ref] = el)} key={i}>
+        <Container fluid className="p-md-4 py-4" style={style}>
+          {component}
+        </Container>
+      </div>
+    );
+  };
+
+  scrollTo = ref => () => {
+    this[ref].scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+}
+
+const NavMenu = ({ sections, onClick }) => {
+  const navItems = Object.entries(sections).map(([ref, { label }], i) => (
+    <NavItem key={i}>
+      <NavLink href="#" onClick={onClick(ref)}>
+        {label}
+      </NavLink>
+    </NavItem>
+  ));
+
+  return <Nav fill={true}>{navItems}</Nav>;
+};
+
+const sections = {
+  event: { component: <Event />, label: "Event Details" },
+  airport: { component: <Airport />, label: "Airport Info" },
+  lodgingList: { component: <LodgingList />, label: "Lodging" },
+  foodList: { component: <FoodList />, label: "Food" },
+  activityList: { component: <ActivityList />, label: "Activities" },
+  contactUs: { component: <ContactUs />, label: "Questions?" }
 };
