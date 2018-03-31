791d1ddf63b49d883a8d267ac0c79b32be6a9254
George Song <george@monozuku.com>
2018-03-30 18:19:10 -0700

    Change to shallow state for PureComponent

--- src/components/Event.js
+++ src/components/Event.js
@@ -34,7 +34,7 @@ export default () => (
 );
 
 class Map extends React.PureComponent {
-  state = { mapContainer: { width: 400, height: 300 } };
+  state = { width: 400, height: 300 };
 
   render = () => (
     <div ref={el => (this.mapContainer = el)}>
@@ -42,8 +42,8 @@ class Map extends React.PureComponent {
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2665.5868734347823!2d-114.24015338435312!3d48.07961667921895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5366535871876905%3A0x3088f2289bea6c!2sVista+Linda+Mexican+%26+Catering!5e0!3m2!1sen!2sus!4v1520397563655"
         title="map"
-        width={this.state.mapContainer.width}
-        height={this.state.mapContainer.height}
+        width={this.state.width}
+        height={this.state.height}
         frameBorder="0"
         allowFullScreen
       />
@@ -53,9 +53,9 @@ class Map extends React.PureComponent {
   handleWindowResize = () => {
     const width = this.mapContainer
       ? this.mapContainer.clientWidth
-      : this.state.mapContainer.width;
+      : this.state.width;
     const height = getHeightFromWidth(width);
-    this.setState({ mapContainer: { width, height } });
+    this.setState({ width, height });
   };
 }
 
