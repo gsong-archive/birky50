44208ca68d96297fc9676545ecea48701d6bc4f5
George Song <george@monozuku.com>
2018-04-04 16:51:45 -0700

    Neat window based selector

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { unregister } from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, window.root);
 unregister();
