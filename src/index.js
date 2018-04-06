0650d4c9ad13bd473a74b89c2a2a3ff40c8174ce
George Song <george@monozuku.com>
2018-04-06 09:11:54 -0700

    Get rid of service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import { unregister } from "./registerServiceWorker";
 
 ReactDOM.render(<App />, window.root);
-unregister();
