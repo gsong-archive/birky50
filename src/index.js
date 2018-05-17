1504815a8f1897af558e035dbb3a14ae7343e4b2
George Song <george@damacy.net>
2018-05-17 07:43:52 -0700

    Add react-perf-devtool

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+if (process.env.NODE_ENV === "development") {
+  const { registerObserver } = require("react-perf-devtool");
+  registerObserver();
+}
+
 ReactDOM.render(<App />, window.root);
