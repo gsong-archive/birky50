7ae8592a11c82e6be0b3ad462cdda2c7ff2844b1
George Song <george@damacy.net>
2018-05-21 14:46:55 -0700

    Set up for cosmos

---
+++ cosmos.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  containerQuerySelector: "#root",
+  webpackConfigPath: "react-scripts/config/webpack.config.dev",
+  publicPath: "public",
+  globalImports: ["./src/styles/global"],
+  // Optional: Add this when you start using proxies
+  // proxiesPath: "src/cosmos.proxies",
+};
