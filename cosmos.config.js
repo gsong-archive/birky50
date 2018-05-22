19366a4b4a944808029e8a9f8a7f7fd0b5d9f6db
George Song <george@damacy.net>
2018-05-21 21:31:27 -0700

    Only watch src for cosmos

--- cosmos.config.js
+++ cosmos.config.js
@@ -3,6 +3,7 @@ module.exports = {
   webpackConfigPath: "cosmos.webpack.config.js",
   publicPath: "public",
   globalImports: ["./src/styles/global"],
+  watchDirs: ["src"],
   // Optional: Add this when you start using proxies
   // proxiesPath: "src/cosmos.proxies",
 };
