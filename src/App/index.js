77344f4777ae9fa0809131c4dbdc2ed0bcc49280
George Song <george@damacy.net>
2018-06-01 06:12:09 -0700

    More browser detection goodness

--- src/App/index.js
+++ src/App/index.js
@@ -29,17 +29,17 @@ export default () => {
                 flex: 1;
               `}
             >
-              <Section color="red" mq={px}>
-                <h1>{px}</h1>
+              <Section color="red" mq={pxMq}>
+                <h1>{pxMq}</h1>
               </Section>
-              <Section color="green" mq={em}>
+              <Section color="green" mq={emMq}>
                 <h1>
-                  {em} ({base * multiplier}px)
+                  {emMq} ({base * multiplier}px)
                 </h1>
               </Section>
-              <Section color="blue" mq={rem}>
+              <Section color="blue" mq={remMq}>
                 <h1>
-                  {rem} ({base * multiplier}px)
+                  {remMq} ({base * multiplier}px)
                 </h1>
               </Section>
             </main>
@@ -51,21 +51,51 @@ export default () => {
 };
 
 const multiplier = 40;
-const px = `@media (min-width: ${16 * multiplier}px)`;
-const em = `@media (min-width: ${multiplier}em)`;
-const rem = `@media (min-width: ${multiplier}rem)`;
+const pxMq = `@media (min-width: ${16 * multiplier}px)`;
+const emMq = `@media (min-width: ${multiplier}em)`;
+const remMq = `@media (min-width: ${multiplier}rem)`;
 
-const Header = () => (
-  <header>
-    <h2>device pixel ratio: {window.devicePixelRatio}</h2>
-    <h2>viewport width: {window.innerWidth}px</h2>
-    <h2>base font-size: {getBaseFontSize()}px</h2>
-  </header>
-);
+class Header extends React.Component {
+  state = { em: 0, rem: 0 };
+  em = React.createRef();
+  rem = React.createRef();
+
+  componentDidMount = () => {
+    this.setState({
+      em: getFontSize(this.em.current),
+      rem: getBaseFontSize(this.rem.current),
+    });
+  };
+
+  render = () => (
+    <header>
+      <h2>user-agent: {navigator.userAgent}</h2>
+      <h2>device pixel ratio: {window.devicePixelRatio}</h2>
+      <h2>viewport width: {window.innerWidth}px</h2>
+      <h2>base font-size: {getBaseFontSize()}px</h2>
+      <p
+        ref={this.em}
+        className={css`
+          font-size: 1em;
+        `}
+      >
+        I'm 1em: {this.state.em}px
+      </p>
+      <p
+        className={css`
+          font-size: 1rem;
+        `}
+      >
+        I'm 1rem: {this.state.rem}px
+      </p>
+    </header>
+  );
+}
 
 const Section = styled.div(
   props => css`
     flex: 1;
+    color: white;
     background: ${props.color};
     ${props.mq} {
       opacity: 0.5;
@@ -77,3 +107,5 @@ const flex = css`
   display: flex;
   flex-direction: column;
 `;
+
+const getFontSize = el => parseFloat(window.getComputedStyle(el).fontSize);
--- src/styles/global.js
+++ src/styles/global.js
@@ -3,7 +3,7 @@ import { injectGlobal } from "emotion";
 
 WebFont.load({
   google: {
-    families: ["Lato", "Work Sans"],
+    families: ["Lato"],
   },
 });
 
@@ -22,16 +22,6 @@ injectGlobal`
     font-family: Lato, sans-serif;
     margin: 0;
   }
-
-  h1, h2 {
-    font-family: Work Sans,sans-serif;
-    font-weight: 500;
-    margin: 0 0 .5rem;
-  }
-  h1 { font-size: 2.5rem; }
-  h2 { font-size: 2rem; }
-
-  address { font-style: normal }
 
   // IE 11
   main { display: block }
