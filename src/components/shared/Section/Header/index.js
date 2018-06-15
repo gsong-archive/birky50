4c46dfb7a56f085d8ffac09051727e3a3373f053
George Song <george@damacy.net>
2018-06-15 09:16:41 -0700

    Sticky section header

--- src/components/shared/Section/Header/index.js
+++ src/components/shared/Section/Header/index.js
@@ -14,14 +14,9 @@ type Props = { color: string, children: React.Element<LabelComponent> };
 const SectionHeader = ({ color, children }: Props) => (
   <SectionContext.Consumer>
     {id => (
-      <div
-        className={css`
-          border-top: 8px solid ${color};
-          padding: 16px 24px 0;
-        `}
-      >
+      <Container color={color}>
         <H1 id={`${id}-description`}>{children}</H1>
-      </div>
+      </Container>
     )}
   </SectionContext.Consumer>
 );
@@ -29,11 +24,23 @@ const SectionHeader = ({ color, children }: Props) => (
 const breakpoints = convertListToEm([350]);
 const mq = facepaint(breakpoints.map(bp => `@media (max-width: ${bp}em)`));
 
+const Container = styled.div(
+  props =>
+    css`
+      border-top: 8px solid ${props.color};
+      padding: 16px 24px 0;
+      position: sticky;
+      top: 0;
+      background-color: hsla(0, 0%, 100%, 0.8);
+    `
+);
+
 const H1 = styled.h1(
   css`
     font-weight: normal;
     line-height: 1.1;
-    margin: 0 0 1em;
+    margin: 0 0;
+    padding: 0 0 1em;
   `,
   mq({ fontSize: ["1.75rem", "1.5rem"] })
 );
